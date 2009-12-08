9fbab4688c677ef61bc7baa737aa835e1adaf8c2
Denis Rosset <rosset.denis@gmail.com>
2009-12-08 15:12:19 +0100

    Added shi configuration for ShiBuiChi.

--- mysite/_config.php
+++ mysite/_config.php
@@ -3,10 +3,22 @@
 global $project;
 $project = 'mysite';
 
-global $database;
-$database = "";
+require_once(str_replace('//','/',__DIR__.'/') .'../.shi/shi.php');
 
-require_once("conf/ConfigureFromEnv.php");
+if (_shi('admin_set') > 0)
+        Security::setDefaultAdmin(_shi('admin_user', 'admin_password'));
+
+global $databaseConfig;
+$databaseConfig = array(
+	'type' => _shi('db_type'),
+        'server' => _shi('db_server'),
+        'username' => _shi('db_username'),
+        'password' => _shi('db_password'));
+
+$database = _shi('db_name');
+
+if (_shi('mode') == 'dev')
+        Director::set_environment_type('dev');
 
 // This line set's the current theme. More themes can be
 // downloaded from http://www.silverstripe.com/themes/
