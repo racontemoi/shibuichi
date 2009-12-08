ec0519470093fcded666952634a24a5a15fd9080
Denis Rosset <rosset.denis@gmail.com>
2009-12-08 16:59:38 +0100

    First version of restore.

---
+++ .shi/bin/restore.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once(str_replace('//','/',dirname(__FILE__).'/../shi.php'));
+
+$database = _shi('db_name');
+$server = _shi('db_server');
+$user = _shi('db_username');
+$password = _shi('db_password');
+$mysql = _shi('tool_mysql');
+$dump_file = _shi('web_root') . '/shi_dump/current.sql';
+
+$result = `echo use $database && cat $dump_file | $mysql --user=$user --password=$password --host=$server`;
+echo "echo use $database && cat $dump_file | $mysql --user=$user --password=$password --host=$server";
+echo "<h1>Result</h1><pre>$result</pre>";
+?>
