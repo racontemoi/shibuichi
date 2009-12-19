c74ba7e4ed7716c882699d0d274afd762de2748c
Denis Rosset <rosset.denis@gmail.com>
2009-12-19 18:37:45 +0100

    Corrected indentation and bug in administrator default user/pass in _config.php.

--- mysite/_config.php
+++ mysite/_config.php
@@ -6,19 +6,19 @@ $project = 'mysite';
 require_once(str_replace('//','/',dirname(__FILE__).'/') .'../.shi/shi.php');
 
 if (_shi('admin_set') > 0)
-        Security::setDefaultAdmin(_shi('admin_user', 'admin_password'));
+	Security::setDefaultAdmin(_shi('admin_username'), _shi('admin_password'));
 
 global $databaseConfig;
-$databaseConfig = array(
-	'type' => 'MySQLDatabase',
-        'server' => _shi('db_server'),
-        'username' => _shi('db_username'),
-        'password' => _shi('db_password'),
-        'database' => _shi('db_name')
-);
+$databaseConfig = 
+	array('type' => 'MySQLDatabase',
+	      'server' => _shi('db_server'),
+	      'username' => _shi('db_username'),
+	      'password' => _shi('db_password'),
+	      'database' => _shi('db_name')
+	      );
 
 if (_shi('mode') == 'dev')
-        Director::set_environment_type('dev');
+	Director::set_environment_type('dev');
 
 // This line set's the current theme. More themes can be
 // downloaded from http://www.silverstripe.com/themes/
