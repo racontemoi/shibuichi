b0388d6b026a539add3006b536fa6c3c63ed10c6
Denis Rosset <rosset.denis@gmail.com>
2009-12-22 16:57:14 +0100

    Added name for label field in Metadata CMS tab. Proposal Trac #4878.

--- sapphire/core/model/SiteTree.php
+++ sapphire/core/model/SiteTree.php
@@ -1256,7 +1256,7 @@ class SiteTree extends DataObject implements PermissionProvider,i18nEntityProvid
 					/*, new TreeMultiselectField("MultipleParents", "Page appears within", "SiteTree")*/
 					new CheckboxField("ProvideComments", $this->fieldLabel('ProvideComments')),
 					new LiteralField(
-						"", 
+						"HomepageForDomainLabel", 
 						"<p>" . 
 							_t('SiteTree.NOTEUSEASHOMEPAGE', 
 							"Use this page as the 'home page' for the following domains: 
