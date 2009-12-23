7054706bba14275bf40dcfaab066b84dab6cbfdc
Denis Rosset <rosset.denis@gmail.com>
2009-12-23 11:42:23 +0100

    Added support for database prefixes and suffixes.

--- mysite/_config.php
+++ mysite/_config.php
@@ -9,13 +9,13 @@ if (_shi('admin_set') > 0)
 	Security::setDefaultAdmin(_shi('admin_username'), _shi('admin_password'));
 
 global $databaseConfig;
+$database = _shi('db_name_prefix') . _shi('db_name') . _shi('db_name_suffix');
 $databaseConfig = 
 	array('type' => 'MySQLDatabase',
 	      'server' => _shi('db_server'),
 	      'username' => _shi('db_username'),
 	      'password' => _shi('db_password'),
-	      'database' => _shi('db_name')
-	      );
+	      'database' => $database);
 
 if (_shi('mode') == 'dev')
 	Director::set_environment_type('dev');
